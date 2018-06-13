d0856936132bfbf28125c6ec7c7771d5f7ef2067
Mark Foege <mfoege@colvosgroup.com>
2018-06-12 20:45:42 -0700

    Is designable and inspectable now. right side pips not drawing at all. UW card theme

--- PlayingCard - mrf/PlayingCardView.swift
+++ PlayingCard - mrf/PlayingCardView.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
+@IBDesignable
 class PlayingCardView: UIView
 {
-    
-    var rank: Int = 11 { didSet { setNeedsDisplay(); setNeedsLayout() } }
-    var suit: String = "♥️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
-    var isFaceUp: Bool = true { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
+    var rank: Int = 7 { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
+    var suit: String = "♦️" { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    @IBInspectable
+    var isFaceUp: Bool = false { didSet { setNeedsDisplay(); setNeedsLayout() } }
     
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
@@ -109,8 +112,16 @@ class PlayingCardView: UIView
         UIColor.white.setFill()
         roundedRect.fill()
         
-        if let faceCardImage = UIImage(named: rankString+suit) {
-            faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+        if isFaceUp {
+            if let faceCardImage = UIImage(named: rankString+suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBoundsSize))
+            } else {
+                drawPips()
+            }
+        } else {
+            if let cardBackImage = UIImage(named: "cardback", in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
+                cardBackImage.draw(in: bounds)
+            }
         }
     }
 }
